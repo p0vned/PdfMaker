246c109b4ebf0b0f744fb6e72a99f29dc0794a0d
Wojciech S <strzebonsky@gmail.com>
2020-08-12 20:45:37 +0200

    Added import file command class

---
+++ PdfMaker/Models/Commands/ImportFileCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PdfMaker.Models.Commands
+{
+    class ImportFileCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
